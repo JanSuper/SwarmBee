d15ef16b86396acadbab0d4637b89bec09e0309b
JanSuper <j.super@student.maastrichtuniversity.com>
2021-11-28 15:30:00 +0100

    Temprature integration

--- DroneSwarm/src/Localization/mapping.py
+++ DroneSwarm/src/Localization/mapping.py
@@ -2,6 +2,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+GREEN_TEMP = 30
+YELLOW_TEMP = 40
+
 class Plot:
     def __init__(self):
         self.fig = plt.figure()
@@ -11,8 +14,13 @@ class Plot:
         self.ax.set_zlabel('Z Label')
 
     def updateGraph(self,xs,ys,zs,yaw,temp):
-        #TODO: integrate temprature and yaw
-        self.ax.scatter(xs, ys, zs,marker='>')
+        #TODO: integrate yaw
+        if temp < GREEN_TEMP:
+            self.ax.scatter(xs, ys, zs,marker='>',c="g")
+        elif temp < YELLOW_TEMP:
+            self.ax.scatter(xs, ys, zs, marker='>', c="y")
+        else:
+            self.ax.scatter(xs, ys, zs, marker='>', c="r")
         plt.pause(0.01)
         return
 
