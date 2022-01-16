61ed216a41092151817422d666ec0e9a1b78129c
Jan Super <j.super@student.maastrichtuniversity.nl>
2022-01-16 11:54:31 +0100

    comment out obstacle avoidance from APID

--- DroneSwarm/src/Control/ArucoPID.py
+++ DroneSwarm/src/Control/ArucoPID.py
@@ -96,8 +96,8 @@ class APID():
         # print(trans)
         trans = list(np.around(np.array(trans), decimals=1))
         # print(trans)
-        if self.obstacleFound:
-           trans = self.avoidObstacles(trans)
+        # if self.obstacleFound:
+        #    trans = self.avoidObstacles(trans)
         return trans
 
     def avoidObstacles(self, trans):
